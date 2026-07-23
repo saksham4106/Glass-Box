a7a7cb24b601162e141ea69247a57597d8c41970
saksham4106 <saksham.vvip@gmail.com>
2026-07-24 03:13:25 +0530

    Object Graph Rendering

---
+++ glassbox-backend/src/main/java/com/saksham4106/glassboxbackend/GlassBoxApplication.java
@@ -0,0 +1,13 @@
+package com.saksham4106.glassboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassBoxApplication.class, args);
+	}
+
+}
